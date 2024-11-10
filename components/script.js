fb77848bda6d38eaa79dc5f658535c7492b8c332
hhh <temporary@example.com>
2024-11-10 23:46:44 +0900

    chore. remove unused files

--- components/script.js
+++
@@ -1,9 +0,0 @@
-function toggleButton(buttonId) {
-    const button = document.getElementById(buttonId);
-    // Remove 'active' class from all buttons
-    document.querySelectorAll('.button').forEach(function(btn) {
-      btn.classList.remove('active');
-    });
-    // Add 'active' class to the clicked button
-    button.classList.add('active');
-}
